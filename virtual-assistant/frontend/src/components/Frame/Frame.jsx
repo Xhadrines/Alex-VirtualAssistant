33b9b51c097df86354e63b69f9eb6d3d28739a46
Xhadrines <sandru99alex@gmail.com>
2025-02-06 12:50:35 +0200

    Modificare frontend + gitignore
    Am modificat frontend-ul, ca atunci cand ruleaza sa afiseze pagina universitatii.
    Am modificat fisierul .gitignore si lam adaugat in backend si frontend.

---
+++ virtual-assistant/frontend/src/components/Frame/Frame.jsx
@@ -0,0 +1,13 @@
+import "./Frame.css";
+
+const Frame = () => {
+  return (
+    <iframe 
+      src="https://fiesc.usv.ro/?r"
+      title="FIESC USV"
+      className="iframe-container"
+    />
+  );
+};
+
+export default Frame;
---
+++ virtual-assistant/frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Frame from "../../components/Frame/Frame";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Frame />
+    </div>
+  );
+};
+
+export default Home;
